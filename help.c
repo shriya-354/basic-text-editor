769bff197761ee55b2b4f87b90160ca0ca4f9fca
shriya-354 <spicyshriya354@gmail.com>
2016-11-06 12:15:44 +0000

    Add files via upload

---
+++ help.c
@@ -0,0 +1,46 @@
+/***********************************************************************************************************************************************
+							       HELP PAGE
+***********************************************************************************************************************************************/
+This is a project to implement my own text editor in C, using the ncurses library. 
+
+The following commands are to be used to implement a task : 
+
+To compile the project, type : make
+
+To open a file : ./project <filename>
+
+To exit : <esc> : <q>
+
+To save a file : <esc> : <w>
+
+To move the cursor left : use left arrow key 
+
+To move the cursor right : use right arrow key 
+
+To move the cursor up : use up arrow key 
+
+To move the cursor down : use down arrow key
+
+To go to the next line  : press <ENTER>
+
+To start inserting press <i>
+
+To delete the character : press x
+
+To set bold font : press B
+
+To delete a word : press w
+
+To delete a line : press l
+
+To delete at the end: press e
+
+To replace a char : press r
+
+To replace a line : press R
+
+
+
+
+
+
